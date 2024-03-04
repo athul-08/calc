6653d76897cba738b09fe834fe9384b192cf2a32
athul-08 <athulprasad0812@gmail.com>
2024-03-04 12:34:07 +0530

    first commit

---
+++ assets/calculator.js
@@ -0,0 +1,26 @@
+let display = document.getElementById('display');
+let expression = '';
+
+function appendNumber(number) {
+    expression += number;
+    display.value = expression;
+}
+
+function appendOperator(operator) {
+    expression += operator;
+    display.value = expression;
+}
+
+function calculate() {
+    try {
+        expression = eval(expression);
+        display.value = expression;
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function clearDisplay() {
+    expression = '';
+    display.value = '';
+}
